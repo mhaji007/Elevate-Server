3b40888e6b7ab3994b637000c2c39b6736c0354a
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-06-04 12:07:36 -0400

    Implement logout route

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,7 +45,7 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
 
     // Check if user exists in database
-    const User = await User.findOne({ email }).exec();
+    const user = await User.findOne({ email }).exec();
     if (!user) res.status(404).send("No user found");
 
     // Compare passwords
@@ -73,3 +73,14 @@ export const login = async (req, res) => {
     return res.status(400).send("Error. Try again.");
   }
 };
+
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.json({message:"Signout success"})
+  }
+  catch(err) {
+    console.log(err)
+  }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,10 @@ const router = express.Router();
 // Import validators
 
 // Import controllers
-import { register, login } from "../controllers/auth";
+import { register, login, logout } from "../controllers/auth";
 
 router.post("/register", register)
 router.post("/login", login)
+router.get("/logout", logout)
 
 module.exports = router;
